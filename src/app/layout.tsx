fc3682a1433444d6dbe66f1e21cc84a6d15b89fe
Kittitorn Sermthaisong <kittitorn.match@gmail.com>
2026-09-17 13:57:50 +0700

    clear code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,12 +43,15 @@ export const viewport: Viewport = {
 };
 
 export const metadata: Metadata = {
-  icons: [
-    {
-      rel: 'icon',
-      url: `${CONFIG.assetsDir}/favicon.ico`,
-    },
-  ],
+  manifest: '/favicon/site.webmanifest',
+  icons: {
+    icon: [
+      { url: '/favicon/favicon.ico' },
+      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [{ url: '/favicon/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }],
+  },
   openGraph: {
     images: [OG_IMAGE_URL],
   },
