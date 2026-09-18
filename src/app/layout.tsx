7abc574bd61492aab7476f4d91b408b08c2765f5
Kittitorn Sermthaisong <kittitorn.match@gmail.com>
2026-09-18 18:16:12 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ const AuthProvider =
   JwtAuthProvider;
 
 const OG_IMAGE_URL =
-  'https://res.cloudinary.com/dkdbilwtj/image/upload/v1781623827/og-images_wrlxnc.jpg';
+  'https://res.cloudinary.com/dkdbilwtj/image/upload/v1789729924/og-image_fu0mob.png';
 
 export const viewport: Viewport = {
   width: 'device-width',
